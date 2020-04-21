6b854534376be85245ebbd409de3a99afbd54020
jessica saenz <jessicasaenz@jessicas-Mac-Pro.local>
2020-04-21 15:58:14 +1000

    remove player class and game_logo method to seperate files

--- main.rb
+++ main.rb
@@ -1,31 +1,5 @@
-require "tty-prompt"
-require "tty-font"
-require "pastel"
-
-font = TTY::Font.new(:doom)
-prompt = TTY::Prompt.new
-pastel = Pastel.new
+require_relative 'print'
 
 
-#Game logo method to print to terminal
-def game_logo
-    pastel = Pastel.new
-    font = TTY::Font.new(:doom)
-    pastel.yellow(font.write("BATTLE WARS"))
-end
 
-class Player
-
-    attr_accessor :player_name
-
-    def initialize
-        @player_name = []
-    end
-#method to get player name from user
-    def player_name
-        prompt = TTY::Prompt.new
-        username = prompt.ask('Enter a Username', default: ENV['Jess'])
-        @player_name.push(username)
-    end
-
-end
+game_logo()
