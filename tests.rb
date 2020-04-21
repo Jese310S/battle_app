0ec6c5946c31ea747a4614aa030f1c98a252514a
jessica saenz <jessicasaenz@jessicas-Mac-Pro.local>
2020-04-21 14:45:55 +1000

    add test for player name

--- tests.rb
+++ tests.rb
@@ -1,10 +1,20 @@
 require 'rspec/autorun'
 require_relative './main'
 
-describe player_name do
-    it "will save username to a variable" do
-    player = player_name()
-    expect(player).to eq("jess")
+describe Player do
+    it "will create a new player object" do
+    player = Player.new
+    class_check = player.is_a?(Player)
+    expect(class_check).to eq(true)
+    end
+
+    it "will add given name to player_name" do
+        player = Player.new
+        player.name()
+        name = player.player_name[0]
+        expect(name).to eq("jess")
     end
 end
+
+
 
