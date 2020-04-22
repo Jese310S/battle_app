e3632aeb5b0bdd5037322ee5d37e9bf3188f1671
Jese310S <jess.sole710@gmail.com>
2020-04-22 23:50:12 +1000

    add attacks method and player attack method to player.rb

--- attack.rb
+++ attack.rb
@@ -1,10 +1,14 @@
-require_relative 'player'
-require_relative 'welcome'
-require_relative 'army'
+require_relative './dragon'
+require_relative './player'
+require_relative './welcome'
+require_relative './army'
 require 'tty-prompt'
+require 'tty-progressbar'
+require 'tty-font'
+require 'pastel'
 
 
-class Attack 
+class Attack < Army
     attr_accessor :name, :hp, :damage, :defense
 
     def initialize(name,hp,damage)
@@ -15,8 +19,24 @@ class Attack
     end
 
     def attacks(opponent)
-        opponent.hp -= @damage
+        opponent.hp -= rand(@damage)
         return opponent.hp
     end
 
+
 end
+
+# def player_attack
+#     dragon = Dragon.new
+#     player = Player.new("jess")
+#     prompt = TTY::Prompt.new
+#     chosen_warrior = prompt.select("Choose your warrior to attack!", %w(Archer Giant Wizard Barbarian ))
+#     dragon_hp = player.attacks(dragon)
+#     font = TTY::Font.new(:straight)
+#     pastel = Pastel.new
+#       puts pastel.purple(font.write("HIT"))
+#     puts pastel.green("Your #{chosen_warrior} hit Draco the Terrible. He has #{dragon_hp}hp left")
+
+# end
+
+# player_attack()
--- player.rb
+++ player.rb
@@ -1,9 +1,13 @@
 require_relative './welcome'
-require_relative 'attack'
-require_relative 'dragon'
+require_relative './attack'
+require_relative './dragon'
+require 'pastel'
+require 'tty-font'
+require 'tty-progressbar'
+require 'tty-prompt'
 
 
-class Player < Attack
+class Player
     attr_accessor :name, :hp, :damage, :defense
 
     def initialize(name)
@@ -13,10 +17,36 @@ class Player < Attack
         @defense = 20
     end
 
+    def attacks(opponent)
+        opponent.hp -= rand(@damage)
+        return opponent.hp
+    end
 
 end
 
-# player = Player.new
-# player.name()
-# puts player.player_name
-# puts player.is_a?(Player)
+def player_attack
+    dragon = Dragon.new(200)
+    player = Player.new("jess")
+    prompt = TTY::Prompt.new
+
+    chosen_warrior = prompt.select("Choose your warrior to attack!", %w(Archer Giant Wizard Barbarian ))
+    puts chosen_warrior
+    dragon_hp = player.attacks(dragon)
+    puts dragon_hp
+    lost_health = dragon_hp/200
+    puts lost_health.to_f
+    puts 
+    pastel = Pastel.new
+    font = TTY::Font.new(:straight)
+    puts pastel.on_green.bold(font.write("-------HIT-------"))
+    bar = TTY::ProgressBar.new("HP [:bar] :percent", total: 30)
+    30.times do
+    sleep(0.1)
+    bar.advance(lost_health)
+end
+   #puts pastel.green.bold("Your #{chosen_warrior} hit Draco the Terrible. He has #{dragon.hp}hp left")
+    return dragon.hp
+
+end
+
+player_attack()
