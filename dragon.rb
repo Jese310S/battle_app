085838bb0edd7fcd24e5e22fa0af8dda06d5a012
Jese310S <jess.sole710@gmail.com>
2020-04-22 23:50:43 +1000

    add display_name and display

--- dragon.rb
+++ dragon.rb
@@ -1,16 +1,48 @@
-require_relative 'attack'
-require_relative 'dragon'
+require 'tty-font'
+require 'terminal-table'
+require 'pastel'
+require 'tty-prompt'
 
-class Dragon < Attack
-    attr_accessor :hp, :attack_stat, :defense_stat
 
-    def initialize
+
+class Dragon 
+    attr_accessor :hp, :damage, :name, :defense
+
+    def initialize(hp)
         @name = "Draco"
-        @hp = 200
+        @hp = hp
         @damage = 20
         @defense = 25
     end
 
+    #prints stylised name to the terminal
+    def display_name
+      pastel = Pastel.new
+      font = TTY::Font.new(:doom)
+      puts pastel.red(font.write("DRACO THE TERRIBLE"))
+    end
 
+    # prints the table to the screen
+   def display_stats
+    #Table of Hero choices and their attack and defense stats
+    table = Terminal::Table.new do |t|
+        t.headings = ['Type','Dragon']
+        t.add_row ['HP', '200']
+    end
+    puts table
+    end
     
 end
+
+def boss_intro
+    pastel = Pastel.new
+    puts pastel.green("You have encountered")
+    dragon = Dragon.new(200)
+    dragon.display_name
+    dragon.display_stats
+    prompt = TTY::Prompt.new
+      prompt.yes?('Ready for battle?')
+
+end
+
+boss_intro()
