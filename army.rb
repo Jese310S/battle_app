81a1f79fa9e923cc71d10d07427fda73018e926c
jessica saenz <jessicasaenz@jessicas-Mac-Pro.local>
2020-04-22 10:54:31 +1000

    add add_army method to Army class and fix bugs

--- army.rb
+++ army.rb
@@ -1,4 +1,4 @@
-require 'prompt'
+require 'tty-prompt'
 require 'pastel'
 
 
@@ -6,9 +6,27 @@ prompt = TTY::Prompt.new
 pastel = Pastel.new
 
 class Army
-    attr_accessor :army
+    attr_accessor :army_warriors
     def initialize 
-        @army = []
+        @army_warriors = []
+    end
+
+
+    #method to print army choices to terminal and store in army variable
+    def choose_army
+        prompt = TTY::Prompt.new
+        pastel = Pastel.new
+        choices = %w(Archer-attk:25-def:15 Barbarian-attk:10 Giant-attk:30 Wizard-attk:15)
+        army = prompt.multi_select("Choose Your Army. You have 50 points to spend!", choices )
+        return army
+    end
+
+    #method to add users chosen army to the army_warriors instance variable
+    def add_army(chosen_army)
+        chosen_army.each{ |warrior| 
+            @army_warriors.push(warrior)
+        }
     end
 
 end
+
