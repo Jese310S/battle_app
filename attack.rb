f405e713ecbda9c9f35d30e875570cc62503edfd
Jese310S <jess.sole710@gmail.com>
2020-04-24 13:29:20 +1000

    add attacks method with two arguments

--- attack.rb
+++ attack.rb
@@ -1,13 +1,14 @@
+require_relative 'army'
 require 'tty-prompt'
 require 'tty-progressbar'
 require 'tty-font'
 require 'pastel'
 
 
-class Attack
+class Attack < Army
     attr_accessor :name, :hp, :damage
 
-    def initialize(name, hp, damag )
+    def initialize(name, hp, damage )
         @name = name
         @hp = hp
         @damage = damage
@@ -18,12 +19,30 @@ class Attack
         miss = 1
         result = rand(2)
         return result == 0 ? "hit" : "miss"
-        
     end
 
-    def attacks(opponent)
-        remaining_hp = opponent.hp -= rand(@damage)
-        return remaining_hp
+    def attacks(opponent, attack_result)
+        if attack_result == "hit"
+            lost_health = opponent.to_f/200
+            bar = TTY::ProgressBar.new("HP [:bar] :percent", total: 30)
+            30.times do
+            sleep(0.1)
+            bar.advance(lost_health)
+            end
+            pastel = Pastel.new
+            font = TTY::Font.new(:straight)
+            puts pastel.on_green.bold(font.write("-------HIT-------"))
+
+        else
+            bar = TTY::ProgressBar.new("HP [:bar] :percent", total: 30)
+            30.times do
+            sleep(0.1)
+            bar.advance(opponent.hp)
+            end
+            pastel = Pastel.new
+            font = TTY::Font.new(:straight)
+            puts pastel.on_red.bold(font.write("-------MISS-------"))
+        end
     end
 
     def lost_health(remaining_hp)
@@ -34,5 +53,6 @@ class Attack
 
 end
 
-attack = Attack.new("jess", 100, 20)
-puts attack.hit_or_miss
+# attack = Attack.new("jess", 100, 20)
+# puts attack.hit_or_miss
+# puts attack.lost_health(100)
