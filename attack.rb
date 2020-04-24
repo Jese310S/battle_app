d6b80fdfe521ee99876f3916720b64657974f42c
Jese310S <jess.sole710@gmail.com>
2020-04-24 22:09:55 +1000

    add win method and fix attacks method

--- attack.rb
+++ attack.rb
@@ -6,24 +6,45 @@ require 'pastel'
 
 
 class Attack < Army
-    attr_accessor :name, :hp, :damage
+    attr_accessor :attacker_name, :attacker_hp, :opponent_hp, :damage, :hit_phrases, :miss_phrases
 
-    def initialize(name, hp, damage )
-        @name = name
-        @hp = hp
+    @@hit_phrases = [" is hit in the chest." , 
+                    " could not dodge the attack and suffered damage", 
+                    " is hit and may need a medic"
+                        ]
+    @@miss_phrases = [  " dodged the attack, no damage was done",
+                        " quickly evaded the attack",
+                        " deflected that attack, taking no damage"
+                    ]
+    def initialize(attacker_name, attacker_hp, opponent_hp, damage )
+        @attacker_name = attacker_name
+        @attacker_hp = attacker_hp
         @damage = damage
+        @opponent_hp = opponent_hp
+    #     @hit_phrases = [" is hit in the chest." , 
+    #                     " could not dodge the attack and suffered damage", 
+    #                     " is hit and may need a medic"
+    #                 ]
+    #     @miss_phrases = ["that is a miss, no damage was done",
+    #                      "must be getting tired, they did no damage",
+    #                      "looks like that attack did not work"
+    #                 ]
     end
 
     def hit_or_miss
-        hit = 0
-        miss = 1
         result = rand(2)
         return result == 0 ? "hit" : "miss"
     end
 
-    def attacks(opponent, attack_result)
+
+    def attacks(opponent, attack_result, attacker_damage)
         if attack_result == "hit"
-            lost_health = opponent.to_f/200
+            #calculate damage done and lost health
+            opponent.hp -= attacker_damage
+            lost_health = opponent.hp.to_f/100
+            #puts the random hit phrase to screen
+             puts "#{opponent.name} #{@@hit_phrases[rand(3)]}"
+            #puts the health bar to the screen
             bar = TTY::ProgressBar.new("HP [:bar] :percent", total: 30)
             30.times do
             sleep(0.1)
@@ -34,10 +55,12 @@ class Attack < Army
             puts pastel.on_green.bold(font.write("-------HIT-------"))
 
         else
+            puts "#{opponent.name}#{@@miss_phrases[rand(3)]}"
             bar = TTY::ProgressBar.new("HP [:bar] :percent", total: 30)
             30.times do
             sleep(0.1)
-            bar.advance(opponent.hp)
+            current_health = opponent.hp.to_f/100
+            bar.advance(current_health)
             end
             pastel = Pastel.new
             font = TTY::Font.new(:straight)
@@ -45,14 +68,16 @@ class Attack < Army
         end
     end
 
-    def lost_health(remaining_hp)
-        lost_health = remaining_hp.to_f/200
-        return lost_health
+
+    def wins(opponent)
+        opponent.hp <= 0 ? true : false
     end
 
 
 end
 
-# attack = Attack.new("jess", 100, 20)
+# attack = Attack.new("jess", 100, 0, 25)
 # puts attack.hit_or_miss
-# puts attack.lost_health(100)
+# puts attack.opponent_hp
+# puts attack.miss_phrases
+# puts attack.wins
