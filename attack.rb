91b6206cd4afd1ff212234a1621be96e1d111afa
Jese310S <jess.sole710@gmail.com>
2020-04-25 16:49:15 +1000

    add hit and miss phrases

--- attack.rb
+++ attack.rb
@@ -21,14 +21,7 @@ class Attack < Army
         @attacker_hp = attacker_hp
         @damage = damage
         @opponent_hp = opponent_hp
-    #     @hit_phrases = [" is hit in the chest." , 
-    #                     " could not dodge the attack and suffered damage", 
-    #                     " is hit and may need a medic"
-    #                 ]
-    #     @miss_phrases = ["that is a miss, no damage was done",
-    #                      "must be getting tired, they did no damage",
-    #                      "looks like that attack did not work"
-    #                 ]
+
     end
 
     def hit_or_miss
@@ -39,32 +32,37 @@ class Attack < Army
 
     def attacks(opponent, attack_result, attacker_damage)
         if attack_result == "hit"
+            pastel = Pastel.new
+            font = TTY::Font.new(:straight)
             #calculate damage done and lost health
             opponent.hp -= attacker_damage
             lost_health = opponent.hp.to_f/100
-            #puts the random hit phrase to screen
-             puts "#{opponent.name} #{@@hit_phrases[rand(3)]}"
+
             #puts the health bar to the screen
             bar = TTY::ProgressBar.new("HP [:bar] :percent", total: 30)
             30.times do
             sleep(0.1)
             bar.advance(lost_health)
             end
-            pastel = Pastel.new
-            font = TTY::Font.new(:straight)
+ 
             puts pastel.on_green.bold(font.write("-------HIT-------"))
+            #puts the random hit phrase to screen
+            puts pastel.green.bold("#{opponent.name} #{@@hit_phrases[rand(3)]}")
+
 
         else
-            puts "#{opponent.name}#{@@miss_phrases[rand(3)]}"
+            pastel = Pastel.new
+            font = TTY::Font.new(:straight)
+
             bar = TTY::ProgressBar.new("HP [:bar] :percent", total: 30)
             30.times do
             sleep(0.1)
             current_health = opponent.hp.to_f/100
             bar.advance(current_health)
             end
-            pastel = Pastel.new
-            font = TTY::Font.new(:straight)
+
             puts pastel.on_red.bold(font.write("-------MISS-------"))
+            puts pastel.green.bold"#{opponent.name}#{@@miss_phrases[rand(3)]}"
         end
     end
 
@@ -73,6 +71,9 @@ class Attack < Army
         opponent.hp <= 0 ? true : false
     end
 
+    def is_dead(character)
+        character.hp <= 0 ? true : false
+    end
 
 end
 
