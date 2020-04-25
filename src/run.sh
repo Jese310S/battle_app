cd47fcac5f226c7cefdb99138cdc470b8811bce7
Jese310S <jess.sole710@gmail.com>
2020-04-25 21:17:45 +1000

    add run.sh script to src folder

---
+++ src/run.sh
@@ -0,0 +1,15 @@
+echo "Welcome to **BATTLE WARS**"
+
+echo "Lets begin the install now"
+
+git clone "https://github.com/Jese310S/battle_app"
+
+cd src
+
+echo "Time to ensure all required gems are installed"
+
+bundle install
+
+echo "All gems have been installed, running application for the first time now"
+
+ruby main.rb
